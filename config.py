540283981e6e9d13f8098b5129dab22d50fe4d07
Manik Surtani <manik@surtani.org>
2010-11-20 03:31:55 +0000

    Made the cfg more generic

--- config.py
+++ config.py
@@ -3,7 +3,3 @@ ALLOWED_REPOS = ['https://github.com/USER1/PROJECT1', 'https://github.com/USER2/
 ### Add your sender email.  Remember, senders need to be admins on your AppEngine account.
 EMAIL_FROM="Me <me@company.com>"
 
-ALLOWED_REPOS = ['https://github.com/maniksurtani/github-emailhook', 'https://github.com/maniksurtani/githelpers', 'https://github.com/infinispan/infinispan'] 
-### Add your sender email.  Remember, senders need to be admins on your AppEngine account.
-EMAIL_FROM="GitHub Email Hook <github@infinispan.org>"
-
