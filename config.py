a578ea1e3240cb2a33714ddc5a9bfef5560a890b
Manik Surtani <manik@squareup.com>
2015-04-17 15:58:37 -0700

    Cfgs for Infinispan

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 ### Add your list of permitted repositories shere
-ALLOWED_REPOS = ['https://github.com/USER1/PROJECT1', 'https://github.com/USER2/PROJECT2'] 
+ALLOWED_REPOS = ['https://github.com/infinispan'] 
 ### Alternatively, add your list of permitted organisations here
-ALLOWED_ORGS = ['ORG1', 'ORG2']
+ALLOWED_ORGS = ['infinispan']
 ### Add your sender email.  Remember, senders need to be admins on your AppEngine account.
-EMAIL_FROM = "Me <me@company.com>"
+EMAIL_FROM = "Infinispan's IMPROVED GitHub Bot <github@infinispan.org>"
 ## Robots. Ignore commits by robots.
-ROBOTS = ["robot@company.com"]
+ROBOTS = ["release@infinispan.org"]
